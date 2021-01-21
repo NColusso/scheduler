d1968db7ad65f79ae72da16d8d7a344d1c0045cd
Natasha Colusso <natashacolusso@Natashas-MacBook-Pro.local>
2021-01-20 17:30:26 -0800

    Added Appointment component and refactored previous components

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import "components/Appointment";
+import Appointment from "components/Appointment";
 
-const days = [
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Natasha",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Alex Smith",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
+    },
   },
 ];
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8001/api/days")
+      .then((response) => setDays(response.data));
+  }, []);
+
+  const appointmentList = appointments.map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ));
+
+  const lastAppt = <Appointment key="last" time="5pm" />;
+
+  const fullAppointments = (
+    <>
+      {appointmentList} {lastAppt}
+    </>
+  );
   return (
     <main className="layout">
       <section className="sidebar">
@@ -41,9 +92,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      <section className="schedule">{fullAppointments}</section>
     </main>
   );
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { action } from "@storybook/addon-actions/dist/preview";
 
 import "components/InterviewerListItem.scss";
@@ -9,13 +9,13 @@ export default function InterviewerListItem(props) {
     "interviewers__item--selected": props.selected,
   });
 
-  const imageClass = classNames("interviewers__item-image", {
-    "interviewers__item-image--selected": props.selected,
-  });
-
   return (
-    <li className={interviewerClass} onClick={props.onClick}>
-      <img className={imageClass} src={props.avatar} alt={props.name} />
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
       {props.selected && props.name}
     </li>
   );
