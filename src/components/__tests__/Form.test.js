01436530d5f8e80fa42b565a4cab6cf59b2b6189
Natasha Colusso <natashacolusso@gmail.com>
2021-01-27 21:14:25 -0800

    Jest test: changing test to pass with addition of check for interviewer when booking

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -48,7 +48,7 @@ describe("Form", () => {
     const onSave = jest.fn();
 
     const { getByText, getByPlaceholderText, queryByText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
+      <Form interviewers={interviewers} onSave={onSave} interviewer={2} />
     );
 
     fireEvent.click(getByText("Save"));
@@ -62,7 +62,7 @@ describe("Form", () => {
 
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 2);
   });
 
   it("calls onCancel and resets the input field", () => {
